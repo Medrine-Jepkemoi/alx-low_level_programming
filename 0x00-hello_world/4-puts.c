13644ed2fc0de8e607d72838b2765679ae09e1f6
Medrine-Jepkemoi <jepkemoimedrine@gmail.com>
2022-03-15 00:38:20 +0300

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,12 @@
- (13 sloc) 257 Bytes
-#include <stdio.h>
+#include <stdio.h>/**
 
-/**
- * main - Entry point
- *
- * Description: prints "Programming is like building a multilingual puzzle"
- *
- * Return: Always 0 (Success)
+    main - prints the phrase Programming is like building a multilingual puzzle
+    Return: 0 if exited properly, non-zero otherwise
+
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
+    
