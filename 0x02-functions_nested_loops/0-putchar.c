6fa3bdad13babbc2e4fad270f74d1b962c5be7d6
Medrine Jepkemoi <medrinejepkemoi@Medrines-MacBook-Air.local>
2022-06-27 18:09:23 +0300

    pritnf() function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - prints _putchar followed by a new line
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+
 int main()
 {
     printf("_putchar\n");
