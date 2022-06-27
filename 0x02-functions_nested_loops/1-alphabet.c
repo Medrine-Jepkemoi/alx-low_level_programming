cddd4a27e71429c92343fb4adf580e05254f6719
Medrine Jepkemoi <medrinejepkemoi@Medrines-MacBook-Air.local>
2022-06-27 18:54:51 +0300

    putchar() function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,10 +7,10 @@ void print_alphabet(void)
     char ch;
     for(ch = 'a'; ch <= 'z'; ch++)
     {
-        putchar(ch);
+        _putchar(ch);
     }
 
-    putchar('\n');
+    _putchar('\n');
 
 }
 
