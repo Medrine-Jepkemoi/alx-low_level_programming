9e1e701a601e84ab9167e5c5ca4974e5f9d835a2
Medrine Jepkemoi <medrinejepkemoi@Medrines-MacBook-Air.local>
2022-06-29 08:22:49 +0300

    Function that checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+    int answer = _islower(c);
+    
+    if (answer = 1)
+    {
+        printf("The character %d is a lowercase character", c);
+    }
+    else
+    {
+        printf("The character %d is not a lowercase character", c);
+    }
+
+    return answer;
+}
