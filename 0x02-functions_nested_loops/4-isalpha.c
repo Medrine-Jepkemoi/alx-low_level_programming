f65c287cc025057d717ab5901ae3a228a4876012
Medrine Jepkemoi <medrinejepkemoi@Medrines-MacBook-Air.local>
2022-06-29 09:43:41 +0300

    function that checks for alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,10 @@
 int _isalpha(int c)
 {
-    if (c >= 'a' && c <= 'Z')
+    if (c >= 'a' && c <= 'z')
+    {
+        return 1;
+    }
+    else if (c >= 'A' && c <= 'Z')
     {
         return 1;
     }
