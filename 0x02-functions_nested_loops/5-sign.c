5dd94c4af45e81a1c8344127aa48a87b75686484
Medrine Jepkemoi <medrinejepkemoi@Medrines-MacBook-Air.local>
2022-06-29 14:59:19 +0300

    function that prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,43 +5,16 @@ int print_sign(int n)
     if (n > 0)
     {
         return 1;
-        putchar('+');
+        _putchar('+');
     }
     else if (n == 0)
     {
         return 0;
-        putchar('0');
+        _putchar('0');
     }
     else
     {
         return -1;
-        putchar('-');
+        _putchar('-');
     }
-}
-
-int main(void)
-{
-    int r;
-
-    r = print_sign(98);
-    putchar(',');
-    putchar(' ');
-    putchar(r + '0');
-    putchar('\n');
-    r = print_sign(0);
-    putchar(',');
-    putchar(' ');
-    putchar(r + '0');
-    putchar('\n');
-    r = print_sign(0xff);
-    putchar(',');
-    putchar(' ');
-    putchar(r + '0');
-    putchar('\n');
-    r = print_sign(-1);
-    putchar(',');
-    putchar(' ');
-    putchar(r + '0');
-    putchar('\n');
-    return (0);
 }
